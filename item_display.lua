dd6624c054541815f7c05b7cbf263a7781381a10
Kyle Ghaby <61133492+kyleghaby@users.noreply.github.com>
2022-11-04 20:47:33 -0500

    Update item_display.lua

--- item_display.lua
+++ item_display.lua
@@ -1,23 +1,29 @@
 
 local GPU = component.gpu
 local ME = component.me_interface
-local refreshtime=60 --s
+local refreshtime=5 --s
+
 
 
 local stats_fh = io.open("stats.dat","w")
-stats_fh.write('Name\t\t\tOld Amount\t\t\tNew Amount\n')
+stats_fh:write('Name        Old Amount      New Amount\n')
 
-local lastUpdate = Computer.uptime() 
+lastUpdate = computer.uptime() 
 local old_size=-1
 --iterate through items 
-if computer.uptime() - lastUpdate > refreshtime then
-    local lastUpdate = computer.uptime()
-    local total_types=#ME.getItemsInNetwork()
-    local item_iter=ME.allItems()
-    for i = 1, total_types, 1 do
-        local i=item_iter()
-        stats_fh.write(i.label,'\t\t\t',old_size,'\t\t\t',i.size,'\n')
-    end
+while true do
+    if computer.uptime() - lastUpdate > refreshtime then
+        print("Refreshed")
+        lastUpdate = computer.uptime()
+        local total_types=#ME.getItemsInNetwork()
+        local item_iter=ME.allItems()
+        for i = 1, total_types, 1 do
+            local i=item_iter()
+            stats_fh:write(i.label,'\t\t\t',old_size,'\t\t\t',i.size,'\n')
+        end
+     end
+    print(computer.uptime() - lastUpdate)
+    os.sleep(20)
  end
     
 
