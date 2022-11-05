32e5fd881099cefe5b9663d25c7ba5f5e0550b1b
Kyle Ghaby <61133492+kyleghaby@users.noreply.github.com>
2022-11-05 00:29:34 -0500

    Update item_display.lua

--- item_display.lua
+++ item_display.lua
@@ -26,21 +26,25 @@ while true do
             i=item_iter()
             local name=i.label
             local new_size=i.size
-            --get old x from last cycle
-            if item_table[name].size then
-                old_size=item_table[name].size
+            if item_table[name] then
+                --get old x from last cycle
+                if item_table[name].size then
+                    old_size=item_table[name].size
+                else
+                    old_size=new_size
+                end
+                local new_dsize=new_size - old_size
+                --get old dx from last cycle
+                if item_table[name].dsize then
+                    old_dsize=item_table[name].dsize
+                else
+                    old_dsize=new_dsize
+                end
+                local d2size=new_dsize - old_dsize
             else
-                old_size=new_size
+                local new_dsize=0
+                local d2size=0
             end
-            local new_dsize=new_size - old_size
-            --get old dx from last cycle
-            if item_table[name].dsize then
-                old_dsize=item_table[name].dsize
-            else
-                old_dsize=new_dsize
-            end
-            local d2size=new_dsize - old_dsize
-            
             item_table[name]={size=new_size, dsize=new_dsize, d2size=d2size}
             if name == 'Plastic Circuit Board' then
                 stats_fh:write(name, size, dsize, new_dsize, d2size'\n')
