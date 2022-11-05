174f3f1735e7eda00211b9d69c136aa2e69e0aec
Kyle Ghaby <61133492+kyleghaby@users.noreply.github.com>
2022-11-04 20:31:29 -0500

    Update item_display.lua

--- item_display.lua
+++ item_display.lua
@@ -1,22 +1,22 @@
-Graphics = require("graphics.graphics")
-GPU = component.gpu
-ME = component.me_interface
-refreshtime=60 --s
 
-    
-stats_fh = io.open("stats.dat",write)
+local GPU = component.gpu
+local ME = component.me_interface
+local refreshtime=60 --s
+
+
+local stats_fh = io.open("stats.dat","w")
 stats_fh.write('Name\t\t\tOld Amount\t\t\tNew Amount\n')
 
 local lastUpdate = Computer.uptime() 
-old_size=-1
+local old_size=-1
 --iterate through items 
 if computer.uptime() - lastUpdate > refreshtime then
-    lastUpdate = computer.uptime()
-    total_types=#ME.getItemsInNetwork()
-    item_iter=ME.allItems()
+    local lastUpdate = computer.uptime()
+    local total_types=#ME.getItemsInNetwork()
+    local item_iter=ME.allItems()
     for i = 1, total_types, 1 do
-        i=item_iter()
-        stats_fh.write(i.label,'\t\t\t',old_size,'\t\t\t',i.size\n)
+        local i=item_iter()
+        stats_fh.write(i.label,'\t\t\t',old_size,'\t\t\t',i.size,'\n')
     end
  end
     
