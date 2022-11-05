3fcc9fdda2e40823da6e27ef7b7fecbe191d20fe
Kyle Ghaby <61133492+kyleghaby@users.noreply.github.com>
2022-11-04 23:32:56 -0500

    Update item_display.lua

--- item_display.lua
+++ item_display.lua
@@ -4,27 +4,27 @@ local computer = require("computer")
 local GPU = component.gpu
 local ME = component.me_interface
 local refreshtime=5 --s
-item_table={}
-i=""
-old_size=0
+local item_table={}
+local i=""
+local old_size=0
 
 
 local stats_fh = io.open("stats.dat","w")
 --stats_fh:write('Name        Old Amount      New Amount\n')
 
-last_update = computer.uptime() 
-initial=true
+local last_update = computer.uptime() 
+local initial=true
 
 while true do
     if initial == true then
         
-        total_types=#ME.getItemsInNetwork()
-        item_iter=ME.allItems()
+        local total_types=#ME.getItemsInNetwork()
+        local item_iter=ME.allItems()
         for n = 1, total_types, 1 do
-            i=item_iter()
-            name=i.label
-            new_size=i.size
-            item_table[name]={old=new_size, new=new_size, dif=0}
+            local i=item_iter()
+            local name=i.label
+            local new_size=i.size
+            local item_table[name]={old=new_size, new=new_size, dif=0}
             if name == 'Plastic Circuit Board' then
                 stats_fh:write(name, '       ', new_size, '       ', new_size, '       ', 0,'\n')
             end
@@ -34,17 +34,17 @@ while true do
     end
     if computer.uptime() - last_update > refreshtime then
         print("Refreshed")
-        last_update = computer.uptime()
-        total_types=#ME.getItemsInNetwork()
-        item_iter=ME.allItems()
+        local last_update = computer.uptime()
+        local total_types=#ME.getItemsInNetwork()
+        local item_iter=ME.allItems()
         --iterate through items
         for n = 1, total_types, 1 do
-            i=item_iter()
-            name=i.label
-            new_size=i.size
-            old_size=item_table[name].old
-            dif=new_size - old_size
-            item_table[name]={old=old_size, new=new_size, dif=dif}
+            local i=item_iter()
+            local name=i.label
+            local new_size=i.size
+            local old_size=item_table[name].old
+            local dif=new_size - old_size
+            local item_table[name]={old=old_size, new=new_size, dif=dif}
             if name == 'Plastic Circuit Board' then
                 stats_fh:write(name, '       ', old_size, '       ', new_size, '       ', new_size - old_size,'\n')
             end
