608f1bf4642483da0eb75eb0d065fe2b096cff9c
Kyle Ghaby <61133492+kyleghaby@users.noreply.github.com>
2022-11-16 12:45:33 -0600

    Update LSC_display.lua

--- power/LSC_display.lua
+++ power/LSC_display.lua
@@ -264,9 +264,9 @@ local oldtime=0
 local didalready=true
 --stats_fh = io.open("stats.dat","w")
  while true do
-    
+    --gpu.fill(1, 1, w, h, " ")
     updateEnergyData(powerStatus)
-    gpu.fill(1, 1, w, h, " ")
+    
     gpu.set(40,1,tostring(energyData.energyPerTick))
     gpu.set(40,2,tostring(energyData.intervalCounter))
     gpu.set(40,3,tostring(energyData.input))
