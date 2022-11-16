502c9eefbead9e196ebe2e8a9b11d8f573470699
Kyle Ghaby <61133492+kyleghaby@users.noreply.github.com>
2022-11-16 13:28:03 -0600

    Update LSC_display.lua

--- power/LSC_display.lua
+++ power/LSC_display.lua
@@ -261,7 +261,7 @@ local oldtime=0
 
 stats_fh = io.open("stats.dat","w")
  while true do
-    --gpu.fill(1, 1, w, h, " ")
+    gpu.fill(1, 1, w, h, " ")
     updateEnergyData(powerStatus)
     --drawEnergyData()
     gpu.set(40,1,tostring(energyData.energyPerTick))
@@ -269,7 +269,7 @@ stats_fh = io.open("stats.dat","w")
     gpu.set(40,3,tostring(energyData.input))
     gpu.set(40,4,tostring(energyData.output))
     gpu.set(40,5,tostring(powerStatus.passiveLoss))
-    gpu.set(40,6,tostring(energyData.input-energyData.input-powerStatus.passiveLoss))
+    gpu.set(40,6,tostring(energyData.input-energyData.output-powerStatus.passiveLoss))
     if energyData.energyIn[energyData.intervalCounter] > 0 then
         stats_fh:write(energyData.intervalCounter,'     ',round((computer.uptime()-oldtime)*20),'     ',energyData.energyIn[energyData.intervalCounter],'\n')
         oldtime=computer.uptime()
