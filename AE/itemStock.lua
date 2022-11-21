7cea1129fb47c2e7942e815beff6a6dcd13722e9
Kyle Ghaby <61133492+kyleghaby@users.noreply.github.com>
2022-11-20 21:37:07 -0600

    Update itemStock.lua

--- AE/itemStock.lua
+++ AE/itemStock.lua
@@ -1,4 +1,4 @@
---HELLO FRIENDS, IF YOU WANT TO ADD AN ITEM THEN ADD TO THE TOP OF THE ITEM STOCKING LIST BELOW. 
+--HELLO FRIENDS, IF YOU WANT TO ADD AN ITEM THEN ADD TO THE TOP OF THE ITEM STOCKING LIST BELOW, REBOOT, AND RUN "autostocker"
 --format: {name="Example",damage="0",checkLvl=10,craftAmt=1000},
     --DONT FORGET THE COMMA
 --damage is the number after the colon in an item's extended name. Only use to fistinguish between 2 items with same name
@@ -6,7 +6,7 @@
 --Essentia and fluids are represented as drops (1 drop = 1 mB)
 
 local itemStock_l={
-    {label="Drilling Fluid Cell",checkLvl=1000,craftAmt=1000}
+    {label="Drilling Fluid Cell",checkLvl=1000,craftAmt=1000},
     {label="Memory (Tier 3.5)",hasTag=false,checkLvl=4,craftAmt=4},
     {label="Internet Card",hasTag=false,checkLvl=1,craftAmt=1},
     {label="Graphics Card (Tier 3)",hasTag=false,checkLvl=1,craftAmt=1},
