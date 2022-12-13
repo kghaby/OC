efbd5724fed64ed19ee05ef93ab631e2f6e80f5c
Kyle Ghaby <61133492+kyleghaby@users.noreply.github.com>
2022-12-12 23:58:45 -0600

    Create EUwatcher.lua

---
+++ misc/EUwatcher.lua
@@ -0,0 +1,9 @@
+local component = require("component")
+local os = require("os")
+local gpu = component.gpu
+
+
+local inputHatchList = {
+    component.proxy("b5c1d2d9-0254-4b47-9582-eab46c49778f"), --wired
+    component.proxy("b30eafff-339f-45dc-8311-04eaafeff7ba")
+} 
